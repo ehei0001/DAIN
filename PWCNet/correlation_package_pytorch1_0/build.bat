9c29237a7fddc8d0b462ff02ce50b05734f5dc3d
이웅주 [ehei] <ehei@nexon.co.kr>
2020-03-04 14:37:46 +0900

    path fixed

--- PWCNet/correlation_package_pytorch1_0/build.bat
+++ PWCNet/correlation_package_pytorch1_0/build.bat
@@ -4,4 +4,4 @@ echo "Need pytorch>=1.0.0"
 rmdir /S /Q build
 rmdir /S /Q correlation_cuda.egg-info
 rmdir /S /Q dist
-..\..\venv\Scripts\python.exe setup.py install
+python.exe setup.py install
--- my_package/build.bat
+++ my_package/build.bat
@@ -5,55 +5,55 @@ cd MinDepthFlowProjection
 rmdir /S /Q build
 rmdir /S /Q MinDepthFlowProjection_cuda.egg-info
 rmdir /S /Q dist
-..\..\venv\Scripts\python.exe setup.py install
+python.exe setup.py install
 cd ..
 
 cd FlowProjection
 rmdir /S /Q build
 rmdir /S /Q FlowProjection_cuda.egg-info
 rmdir /S /Q dist
-..\..\venv\Scripts\python.exe setup.py install
+python.exe setup.py install
 cd ..
 
 cd SeparableConv
 rmdir /S /Q build
 rmdir /S /Q SeparableConv_cuda.egg-info
 rmdir /S /Q dist
-..\..\venv\Scripts\python.exe setup.py install
+python.exe setup.py install
 cd ..
 
 cd InterpolationCh
 rmdir /S /Q build
 rmdir /S /Q InterpolationCh_cuda.egg-info
 rmdir /S /Q dist
-..\..\venv\Scripts\python.exe setup.py install
+python.exe setup.py install
 cd ..
 
 cd DepthFlowProjection
 rmdir /S /Q build
 rmdir /S /Q DepthFlowProjection_cuda.egg-info
 rmdir /S /Q dist
-..\..\venv\Scripts\python.exe setup.py install
+python.exe setup.py install
 cd ..
 
 cd Interpolation
 rmdir /S /Q build
 rmdir /S /Q Interpolation_cuda.egg-info
 rmdir /S /Q dist
-..\..\venv\Scripts\python.exe setup.py install
+python.exe setup.py install
 cd ..
 
 cd SeparableConvFlow
 rmdir /S /Q build
 rmdir /S /Q SeparableConvFlow_cuda.egg-info
 rmdir /S /Q dist
-..\..\venv\Scripts\python.exe setup.py install
+python.exe setup.py install
 cd ..
 
 cd FilterInterpolation
 rmdir /S /Q build
 rmdir /S /Q FilterInterpolation_cuda.egg-info
 rmdir /S /Q dist
-..\..\venv\Scripts\python.exe setup.py install
+python.exe setup.py install
 cd ..
 

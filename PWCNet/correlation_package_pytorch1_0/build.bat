340b8bcb02cca230b5213843147cb1011b20c2d6
ehei <ehei@nexon.co.kr>
2020-03-24 13:22:54 +0900

    fixed to build on RTX2080

--- PWCNet/correlation_package_pytorch1_0/build.bat
+++ PWCNet/correlation_package_pytorch1_0/build.bat
@@ -1,7 +1,7 @@
 echo "Need pytorch>=1.0.0"
-#source activate pytorch1.0.0
+ECHO source activate pytorch1.0.0
 
 rmdir /S /Q build
 rmdir /S /Q correlation_cuda.egg-info
 rmdir /S /Q dist
-python.exe setup.py install
+..\..\venv\Scripts\python.exe setup.py install
--- PWCNet/correlation_package_pytorch1_0/clean.bat
+++ PWCNet/correlation_package_pytorch1_0/clean.bat
@@ -1,5 +1,5 @@
 echo "Need pytorch>=1.0.0"
-#source activate pytorch1.0.0
+ECHO source activate pytorch1.0.0
 
 rmdir /S /Q build
 rmdir /S /Q correlation_cuda.egg-info
